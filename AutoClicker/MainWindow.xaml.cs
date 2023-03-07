e029318a5ac954e8dd12bd96fc40e9597d088fa2
Horst <horst>
2023-03-07 19:08:50 +0100

    changed buttons

--- AutoClicker/MainWindow.xaml.cs
+++ AutoClicker/MainWindow.xaml.cs
@@ -1,39 +1,76 @@
-// Copyright (c) Microsoft Corporation and Contributors.
-// Licensed under the MIT License.
-
+using Microsoft.UI.Input;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using System;
-using System.Collections.Generic;
-using System.IO;
+using System.Threading;
+using Windows.UI.Core;
+using Windows.UI.Input.Preview.Injection;
+using System.Diagnostics;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using Microsoft.UI.Xaml.Input;
+using Windows.System;
 
 namespace AutoClicker
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainWindow : Window
     {
+        private readonly InputInjector injector = InputInjector.TryCreate();
+        private Timer timer;
+        private bool isRunning = false;
+        private int start_delay = 1;
+        private int time_between_clicks = 1;
+
         public MainWindow()
         {
             this.InitializeComponent();
+            this.Title = "AutoClicker";
         }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
+
+        private async void set_start_delay(object sender, RoutedEventArgs e)
         {
-            myButton.Content = "Clicked";
+            if (start_delay_textbox.Text.All(char.IsNumber))
+            {
+                start_delay = int.Parse(start_delay_textbox.Text);
+                Debug.WriteLine("delay is now " + start_delay.ToString());
+            }
+        }
+
+        private async void set_time_between_clicks(object sender, RoutedEventArgs e)
+        {
+            if (time_between_clicks_textbox.Text.All(char.IsNumber))
+            {
+                time_between_clicks = int.Parse(time_between_clicks_textbox.Text);
+                Debug.WriteLine("time between clicks is now " + time_between_clicks.ToString());
+            }
+        }
+
+        private async void startButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (isRunning)
+            {
+                Debug.WriteLine("stopping the timer");
+                timer.Dispose();
+                start_button.Content = "Start";
+            }
+            else {
+                start_button.Content = "Stop";
+                System.Threading.Thread.Sleep(start_delay*1000);
+                Debug.WriteLine("starting the timer");
+                timer = new Timer(TimerCallback, null, TimeSpan.Zero, TimeSpan.FromSeconds(time_between_clicks));
+            }
+            isRunning = !isRunning;
+        }
+
+        private async void TimerCallback(object state)
+        {
+            var input = new InjectedInputMouseInfo();
+            input.MouseOptions = InjectedInputMouseOptions.LeftDown;
+            injector.InjectMouseInput(new[] { input });
+
+            input.MouseOptions = InjectedInputMouseOptions.LeftUp;
+            injector.InjectMouseInput(new[] { input });
+            Debug.WriteLine("click");
         }
     }
 }
